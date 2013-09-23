950c0fdc0cab13ff186af3848b8f67235b2a5923
aces28 <aces28@gmail.com>
2013-09-23 11:24:34 -0400

    "- Initial commit, this is just for an example"

---
+++ src/main/java/com/sudocode/chuva/HelloAndroidActivity.java
@@ -0,0 +1,43 @@
+package com.sudocode.chuva;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class HelloAndroidActivity extends Activity {
+
+    private TextView mTextView;
+    private EditText mEditText;
+
+    /**
+     * Called when the activity is first created.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mEditText = (EditText) findViewById(R.id.editText);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(com.sudocode.chuva.R.menu.main, menu);
+        return true;
+    }
+
+    public void onSayMyName(View view) {
+        Toast.makeText(this, "Hello World " + mEditText.getText(), Toast.LENGTH_SHORT).show();
+    }
+
+}
+
---
+++ target/generated-sources/r/com/sudocode/chuva/BuildConfig.java
@@ -0,0 +1,5 @@
+package com.sudocode.chuva;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = true;
+}
